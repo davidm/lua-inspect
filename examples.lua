f6463c36f52fc07af168088d7b434c94036f43f2
David Manura <dm.git@math2.org>
2010-07-24 00:04:28 -0400

    add examples.lua

---
+++ examples.lua
@@ -0,0 +1,38 @@
+-- some examples/tests
+
+local mt = require "math"
+
+local t = {}
+local t2 = {z=123}
+
+t.abc = 123
+t.def = function() end
+t.abc = {}
+t.abc.def = {x=2}
+t.abc.def =  t.abc.def.x  + 1 + t:def() + t2.z
+
+t2.abc = 456
+t2.def = 234
+
+t3 = {}
+t3.abc = 123
+t3.def = 234
+t3.abc = {}
+t3.abc.def = {x=2}
+t3.abc.def =  t3.abc.def.x  + 1
+t3[123] = {abc=3}
+print(t3[123].abc)
+
+local asdf, asdfg
+print(math.sqrt(2), _G.math.sqrt(2).asd, _G.b:f())
+print(string.lower("A"), string.lowers("A"), string:lower():upper(), asdf, asd)
+print(mt.sqrt(2), mt.sqr(2), math.pi)
+
+local sum = math.pi + 2
+local sum = math.pii + 2
+local sum = math.sqrt(4) + 1
+local sum2 = sum + 1 + math.cos(2*4)^2
+local zz = "Z" .. "Z"
+local str1 = ("aBc"):upper():gsub("A", zz)
+
+
