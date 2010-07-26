a53e241014dfded874768c7bb39fd9e5195bcc54
David Manura <dm.git@math2.org>
2010-07-26 09:14:27 -0400

    SciTE: remove fake filename from loadstring error message.

--- luainspectlib/luainspect/scite.lua
+++ luainspectlib/luainspect/scite.lua
@@ -113,34 +113,35 @@ local function update_ast()
   -- loadstring is much faster than Metalua, so try that first.
   -- Furthermore, Metalua accepts a superset of the Lua grammar.
   local linenum, colnum, err, linenum2
-  local ok, err_ = loadstring(newtextm, "fake.lua") --2DO more
+  local ok, err_ = loadstring(newtextm, "")
   if not ok then
     err = err_
-    linenum = assert(err:match(":(%d+)"))
+    err = err:gsub('^%[string ""%]:', "")
+    linenum = assert(err:match("(%d+):"))
     colnum = 0
     linenum2 = err:match(":%d+: '[^']+' expected %(to close '[^']+' at line (%d+)")
   else
-    local ok_, ast_ = pcall(LI.ast_from_string, newtextm, "fake.lua"); ok = ok_
+    local ok_, ast_ = pcall(LI.ast_from_string, newtextm, "noname.lua"); ok = ok_
     if not ok then
       print "warning: metalua failed to compile code that compiles with loadstring.  error in metalua?"
       err = ast_
       err = err:match('[^\n]*')
       err = err:gsub("^.-:%s*line", "line")
-          -- mlp.chunk prepending this is undesirable.   error(msg,0) would be better in gg.lua.
-	  -- Reported.  TODO: remove when fixed in Metalua.
+          -- mlp.chunk prepending this is undesirable.   error(msg,0) would be better in gg.lua. Reported.
+	  -- TODO-Metalua: remove when fixed in Metalua.
       linenum, colnum = err:match("line (%d+), char (%d+)")
       if not linenum then
         -- Metalua libraries may return "...gg.lua:56: .../mlp_misc.lua:179: End-of-file expected"
         -- without the normal line/char numbers given things like "if x then end end".  Should be
-	-- fixed probably with gg.parse_error in _chunk in mlp_misc.lua.  TODO: remove when
-	-- fixed in Metalua.
+	-- fixed probably with gg.parse_error in _chunk in mlp_misc.lua.
+	-- TODO-Metalua: remove when fixed in Metalua.
         linenum = editor.LineCount - 1
         colnum = 0
       end
     else
       buffer.ast = ast_
     end
-  end -- 2DO: filename
+  end
   --unused: editor.IndicStyle[0]=
   if not ok then
      local pos = linenum and editor:PositionFromLine(linenum-1) + colnum - 1
