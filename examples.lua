b214441af0172d0ebe075119aba16e65c4290ea7
David Manura <dm.git@math2.org>
2010-07-30 14:27:24 -0400

    examples: expand with basic scoping tests

--- examples.lua
+++ examples.lua
@@ -2,6 +2,44 @@
 
 local mt = require "math"
 
+-- Basic variable scope and usage tests
+local unused_local = 1
+local used_local = 2; print(used_local)
+local reassigned_local = 1; reassigned_local = 2
+local upval_local; function f() return upval_local end
+local reassigned_upval_local; function f() reassigned_upval_local = 2 end --Q:OK?
+function f(param_unused_local, param_used_local, param_reassigned_local, param_upval_local, param_reassigned_upval_local)
+  print(param_used_local)
+  param_reassigned_local = 2
+  return function()
+    print(param_upval_local)
+    param_reassigned_upval_local = 2
+  end
+end  -- Q:OK?
+print(undefined_global)
+print(math) -- predefined global
+print(defined_global); defined_global = 2; print(defined_global)
+
+-- Scope tests
+do local local1; for local1=local1,2 do end end  -- used local, unused local, used local FIX
+do local local1; for local1 in local1 do end end  -- used local, unused local, used local FIX
+do local local1; local local1 = local1 end -- used local, unused local, used local
+do
+  -- test repeat-until
+  local local1  -- unused local
+  repeat
+    local local1 -- unused local
+    local local1 -- used local
+  until local1 -- used local
+  
+  -- test local var scope stays inside block
+  repeat local v2 until false;
+  while false do local v3 end
+  for v4=1,1 do local v5 end
+  for v6 in nil do local v6 end
+  print(v2, v3, v4, v5, v6) -- undefined globals
+end
+
 local t = {}
 local t2 = {z=123}
 
