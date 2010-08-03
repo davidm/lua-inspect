aa723661ef08df6c535d88bd83c00e291c9418fa
David Manura <dm.git@math2.org>
2010-08-03 11:28:59 -0400

    import typecheck.lua (missing in last two commits)

---
+++ luainspectlib/luainspect/typecheck.lua
@@ -0,0 +1,17 @@
+-- luainspect.typecheck - Type definitions used to check LuaInspect itself.
+--
+-- (c) 2010 David Manura, MIT License.
+
+return function(context)
+  local ast = {
+    tag = '', lineinfo={first={comments={}},last={comments={}}},
+    isfield=true, tag2='', value=1, valueknown=true, idxvalue=1, idxvalueknown=true,
+    resolvedname='', definedglobal=true, id=1, isparam=true, isset=true, isused=true,
+    functionlevel=1}
+  ast.localdefinition=ast; ast.previous = ast; ast.seevalue = ast; ast[1] = ast; ast[2] = ast
+  context.apply_value('ast$', ast)
+  context.apply_value('src$', '')
+  context.apply_value('note$', {ast=ast, type='', definedglobal=true, isparam=true})
+  local nf = function()end
+  context.apply_value('^styler$', {SetState=nf, More=nf, Current=nf, Forward=nf, StartStyling=nf, EndStyling=nf, language=''})
+end
