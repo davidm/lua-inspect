3cdb2f8db189438ca05e687c50616bdab603d012
David Manura <dm.git@math2.org>
2010-08-31 10:12:40 -0400

    [!] SciTE: fixed default LUAINSPECT_PATH. Reported by Tymur Gubayev.

--- extman/scite_lua/luainspect.lua
+++ extman/scite_lua/luainspect.lua
@@ -1,7 +1,7 @@
 -- This installs LuaInspect in SciTE.
 
 -- If necessary, edit the following path to match your system.
-local LUAINSPECT_PATH = props['ext.lua.directory'] .. '/..'    -- "c:/lua-inspect"
+local LUAINSPECT_PATH = props['ext.lua.directory'] .. '/../..'    -- "c:/lua-inspect"
 package.path = package.path .. ";" .. LUAINSPECT_PATH .. "/metalualib/?.lua"
 package.path = package.path .. ";" .. LUAINSPECT_PATH .. "/luainspectlib/?.lua"
 require "luainspect.scite" : install()
